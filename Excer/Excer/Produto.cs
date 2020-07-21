1d69038988e8a8bb85fce25262830569e0cd3e5d
Miguel <miguelbatista.web@gmail.com>
2020-07-21 19:16:33 -0300

    Sobre Exercício

---
+++ Excer/Excer/Produto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excer
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        // calcula valor total produto estoque
+        public double ValorTotalEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        // Executa soma valor atual + valor de entrada
+        public void AdicionarProdutos(int quantidade)
+        {   // Quantidade = Quantidade + quantidade
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {   // Quantidade = Quantidade - quantidade
+            Quantidade -= quantidade;
+        }
+        // formata saida dos dados do produto para String
+        public override string ToString()
+        {
+            return Nome
+              + ", $ "
+              + Preco.ToString("F2", CultureInfo.InvariantCulture)
+              + ", "
+              + Quantidade
+              + " unidades, Total: $ "
+              + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Excer/Excer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace Excer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Digite o nome produto: ");
+            p.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite a quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            // Ou p.ToString(). p Concatena com string e chama a funçao ToString automaticamente
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Adionar quantidade de produtos: ");
+            int qtde = int.Parse(Console.ReadLine());
+
+            // funçao soma valor atual + entrada do objeto
+            p.AdicionarProdutos(qtde);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados " + p);
+
+            Console.WriteLine();
+            Console.Write("Remover produtos: ");
+            int qtdeRemover = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qtdeRemover);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados " + p);
+
+            Console.ReadKey();
+        }
+    }
+}
