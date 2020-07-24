be64133a7210ab297295ef2392ae3ef982680c4e
Miguel <miguelbatista.web@gmail.com>
2020-07-23 22:32:35 -0300

    Inclui Exercício Circunferência

--- Excer/AdicionaRemoveProduto/Program.cs
+++ Excer/AdicionaRemoveProduto/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Globalization;
-namespace Excer
+namespace AdicionaRemoveProduto
 {
     class Program
     {
