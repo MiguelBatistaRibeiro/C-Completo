d49bceeace06549b7212dd9a5564e9d733fa191b
Miguel <miguelbatista.web@gmail.com>
2020-07-23 23:11:11 -0300

    Tornando Membros Static

--- Excer/RaioCircunferenciaVolumePI/Calculadora.cs
+++ Excer/RaioCircunferenciaVolumePI/Calculadora.cs
@@ -6,14 +6,16 @@ using System.Threading.Tasks;
 
 namespace RaioCircunferenciaVolumePI
 {
+    // Sem modificador de acesso - default > internal - Acessivel a um mesmo assembly - Ex: financeiro
     class Calculadora
     {
-        public double Pi = 3.14;
-        public double Circunferencia(double raio)
+        // Membos estáticos chama direto pelo nome classe em Program. Sem instancia.
+        public static double Pi = 3.14;
+        public static double Circunferencia(double raio)
         {
             return 2.0 * Pi * raio;
         }
-        public double Volume(double raio)
+        public static double Volume(double raio)
         {
             return 4.0 / 3.0 * Pi * Math.Pow(raio, 3.0);
         }
--- Excer/RaioCircunferenciaVolumePI/Program.cs
+++ Excer/RaioCircunferenciaVolumePI/Program.cs
@@ -10,18 +10,16 @@ namespace RaioCircunferenciaVolumePI
       
         static void Main(string[] args)
         {
-            // Instancia objeto para acesso aos membros
-            var calc = new Calculadora();
-
+    
             Console.WriteLine("Entre com o valor do raio: ");
             var valorRaio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = calc. Circunferencia(valorRaio);
-            double volume = calc.Volume(valorRaio);
+            double circ = Calculadora.Circunferencia(valorRaio);
+            double volume = Calculadora.Volume(valorRaio);
 
             Console.WriteLine("Circunferência: " + circ.ToString("F2"), CultureInfo.InvariantCulture);
             Console.WriteLine("Volume: " + volume.ToString("F2"), CultureInfo.InvariantCulture);
-            Console.WriteLine("PI: " + calc.Pi.ToString("F2"), CultureInfo.InvariantCulture);
+            Console.WriteLine("PI: " + Calculadora.Pi.ToString("F2"), CultureInfo.InvariantCulture);
 
             Console.ReadKey();
         }
