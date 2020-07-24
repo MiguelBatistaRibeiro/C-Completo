e917190eaac402d6bd8d1458d94a54583ff72932
Miguel <miguelbatista.web@gmail.com>
2020-07-23 22:49:24 -0300

    Responsabilidade do Calculo Para sua Classe

---
+++ Excer/RaioCircunferenciaVolumePI/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaioCircunferenciaVolumePI
+{
+    class Calculadora
+    {
+        public double Pi = 3.14;
+        public double Circunferencia(double raio)
+        {
+            return 2.0 * Pi * raio;
+        }
+        public double Volume(double raio)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(raio, 3.0);
+        }
+    }
+}
--- Excer/RaioCircunferenciaVolumePI/Program.cs
+++ Excer/RaioCircunferenciaVolumePI/Program.cs
@@ -7,30 +7,24 @@ namespace RaioCircunferenciaVolumePI
 {
     public class Program
     {
-        static double Pi = 3.14;
+      
         static void Main(string[] args)
         {
+            // Instancia objeto para acesso aos membros
+            var calc = new Calculadora();
+
             Console.WriteLine("Entre com o valor do raio: ");
             var valorRaio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = Circunferencia(valorRaio);
-            double volume = Volume(valorRaio);
+            double circ = calc. Circunferencia(valorRaio);
+            double volume = calc.Volume(valorRaio);
 
             Console.WriteLine("Circunferência: " + circ.ToString("F2"), CultureInfo.InvariantCulture);
             Console.WriteLine("Volume: " + volume.ToString("F2"), CultureInfo.InvariantCulture);
-            Console.WriteLine("PI:  = " + circ.ToString("F2"), CultureInfo.InvariantCulture);
+            Console.WriteLine("PI: " + calc.Pi.ToString("F2"), CultureInfo.InvariantCulture);
 
             Console.ReadKey();
         }
-        // Quando uma funçao static exige outra, também deve ser static
-        static double Circunferencia(double raio)
-        {
-            return 2.0 * Pi * raio;
-        }
-        // sem necessidade de public, acessivel apenas para esta class
-        static double Volume(double raio)
-        {
-            return 4.0 / 3.0 * Pi * Math.Pow(raio, 3.0);
-        }
+        
     }
 }
