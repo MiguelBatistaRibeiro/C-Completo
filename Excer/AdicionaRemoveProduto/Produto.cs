c1b6616c684b94cd556a82ad26b46e6466d326d4
Miguel <miguelbatista.web@gmail.com>
2020-07-27 21:25:20 -0300

    Auto Properties

--- Excer/AdicionaRemoveProduto/Produto.cs
+++ Excer/AdicionaRemoveProduto/Produto.cs
@@ -5,45 +5,37 @@ namespace AdicionaRemoveProduto
 {
     class Produto
     {
-        private string _nome;
-        private double _preco;
-        private int _quantidade;
-       public Produto(string nome, double preco, int quantidade) 
+        // PUBLIC NOS ATRIBUTOS, PORÉM PRIVATE NO ACESSADOR SET - NÃO PERMITE ALTERAÇÃO
+        public string Nome { get; private set; }
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto(string nome, double preco, int quantidade) 
         {
-            _nome = nome;
-            _preco = preco;
-            _quantidade = quantidade;
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
         }
-        // ACESSANDO ATRAVÉS DAS PROPETIES - VALUE CORRESPONDE AO PARAMETRO DO SET
-        public string Nome
-        {
-           get { return _nome; }
-           set {
-               if (value != null & value.Length > 1)
-                {
-                    _nome = value;
-                }
-            }     
-        }
+
         public double ValorTotalEstoque()
         {
-            return _preco * _quantidade;
+            return Preco * Quantidade;
         }
         public void AdicionarProdutos(int quantidade)
-        {   
-            _quantidade += quantidade;
+        {
+            Quantidade += quantidade;
         }
         public void RemoverProdutos(int quantidade)
-        {   
-            _quantidade -= quantidade;
+        {
+            Quantidade -= quantidade;
         }
         public override string ToString()
         {
-            return _nome
+            return Nome
               + ", $ "
-              + _preco.ToString("F2", CultureInfo.InvariantCulture)
+              + Preco.ToString("F2", CultureInfo.InvariantCulture)
               + ", "
-              + _quantidade
+              + Quantidade
               + " unidades, Total: $ "
               + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
--- Excer/AdicionaRemoveProduto/Program.cs
+++ Excer/AdicionaRemoveProduto/Program.cs
@@ -9,11 +9,8 @@ namespace AdicionaRemoveProduto
         {
             Produto p = new Produto("TV", 500.00, 10);
 
-            // EFETUANDO O SET 
-            p.Nome = "TV 4K";
-
-            // EFETUANDO O GET 
             Console.WriteLine(p.Nome);
+            
             Console.ReadKey();
         }
     }
