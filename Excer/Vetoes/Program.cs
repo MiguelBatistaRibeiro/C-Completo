52947fcd31391180b2ef08f4369bbaba429fbd2e
Miguel <miguelbatista.web@gmail.com>
2020-07-30 15:34:42 -0300

    Vetor part 2

--- Excer/Vetoes/Program.cs
+++ Excer/Vetoes/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Specialized;
 using System.Globalization;
+using Vetoes;
 
 namespace Vetores
 {
@@ -11,9 +12,9 @@ namespace Vetores
             // Utilizando uma entrada para definir o tamanho do vetor de tipo double e obtendo a média dos números. 
 
             // Pede o tamanho do vetor 
-          Console.WriteLine("Entre com o tamanho da lista: ");
-          int n = int.Parse(Console.ReadLine());
-          double[] vetorNumeros = new double[n];
+            Console.WriteLine("Entre com o tamanho da lista: ");
+            int n = int.Parse(Console.ReadLine());
+            double[] vetorNumeros = new double[n];
 
             // inicia com 0, em quanto < que n, n++
             for (int i = 0; i < n; i++)
@@ -25,13 +26,44 @@ namespace Vetores
             // for para soma dos valores do vet
             double sum = 0.0;
             for (int i = 0; i < n; i++)
-            {   
+            {
                 sum += vetorNumeros[i];
             }
-
             double media = sum / n;
 
             Console.WriteLine("Média dos números = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+            // Fazer um programa para ler um numero inteiro N e os dados (nome e preço) de N Produtos. Armazene
+            // os N produtos em um vetor. Em seguida, mostrar o preço médio dos produtos.
+            Console.WriteLine("ENTRE COM O TAMANHO DA LISTA DE PRODUTOS: ");
+            int qtdProdutos = int.Parse(Console.ReadLine());
+
+            // cria vetor produto recebendo o numeroProdutos
+            Produto[] vetProd = new Produto[qtdProdutos];
+
+            for (int i = 0; i < qtdProdutos; i++)
+            {
+                // A cada chec, pede o nome e preco e insere no vetor produto
+                Console.WriteLine("Insira o nome do Produto: ");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("Insira o preço : ");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                // inicializa as variaveis como n
+                vetProd[i] = new Produto { Nome = nome, Preco = preco };
+            }
+            double soma = 0.0;
+            for (int i = 0; i < qtdProdutos; i++)
+            {
+                soma += vetProd[i].Preco;
+            }
+
+            double mediaProd = sum / qtdProdutos;
+            Console.WriteLine($"A média dos preços é {mediaProd.ToString("F2", CultureInfo.InvariantCulture)}");
+
             Console.ReadKey();
         }
     }
