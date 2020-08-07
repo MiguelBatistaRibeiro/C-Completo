8c531e6ae0f240595b3b60235ab3813d9057d668
Miguel <miguelbatista.web@gmail.com>
2020-08-07 10:27:27 -0300

    Exercício Quartos

---
+++ Excer/HowManyRooms/Hotel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowManyRooms
+{
+    class Hotel
+    {
+      
+        public int Quarto { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public Hotel(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+    }
+}
---
+++ Excer/HowManyRooms/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowManyRooms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hotel[] vect = new Hotel[10];
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Hotel(nome, email);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+            Console.ReadKey();
+
+            string[] vecti = new string[] { "Maria", "Bob", "Alex" };
+            foreach (string obj in vecti)
+            {
+                Console.WriteLine(obj);
+            }
+
+
+        }
+
+    }
+}
