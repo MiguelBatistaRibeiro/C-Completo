e5613a416cb519a9a01240fe1b5fd8645b510662
Migbari <webdevel.bin@gmail.com>
2020-08-25 16:36:57 -0300

    StringBuilder

---
+++ Excer/StringBuilder/Entities/Post.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringBuilder.Entities
+{
+    class Post
+    {
+        
+    }
+   
+}
---
+++ Excer/StringBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using StringBuilder.Entities;
+using System;
+
+namespace StringBuilsder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice dream");
+            Comment c2 = new Comment("Wow that's awesome!");
+
+            Post p1 = new Post(DateTime.Parse("21/05/2018 13:05:44"), "Finaly", "Good night for all of you", 9);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Have a nice weekend");
+            Comment c4 = new Comment("Wow that's wonderful!");
+
+            Post p2 = new Post(DateTime.Parse("22/05/2018 15:30:24"), "Bye Bye", "I'm goig to the house", 9);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
+
+
