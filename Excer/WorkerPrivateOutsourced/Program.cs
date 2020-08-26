27d8d8baebefcd1c0391d707c6d6dddfa9580947
Migbari <webdevel.bin@gmail.com>
2020-08-26 19:02:25 -0300

    Herança + Polimorfismo + Exercício ex

---
+++ Excer/WorkerPrivateOutsourced/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using WorkerPrivateOutsourced.Entities;
+
+namespace EmployeeAndOutsourcedEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.WriteLine("Entre com o número de empregados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Funcionario #{i}");
+                
+                Console.WriteLine("Terceirizado (y/n)? ");
+                char tr = char.Parse(Console.ReadLine());
+                Console.WriteLine("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.WriteLine("Valor por hora: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if(tr == 'y')
+                {
+                    Console.WriteLine("Cobrança adicional");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add( new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pagamentos:");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+            Console.ReadLine();
+        }
+    }
+}
