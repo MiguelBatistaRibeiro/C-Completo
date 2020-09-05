6278ac9ec90c366803e72a7ac01e8dbe308f969a
Migbari <webdevel.bin@gmail.com>
2020-09-05 12:55:14 -0300

    Treina Tudo Xadrez

---
+++ Excer/TreinaTudoXadrez/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TreinaTudoXadrez.tabuleiro;
+
+namespace TreinaTudoXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P = new Posicao(2, 4);  
+
+            Console.WriteLine("Posição: " + P);
+            Console.ReadLine();
+        }
+    }
+} 
