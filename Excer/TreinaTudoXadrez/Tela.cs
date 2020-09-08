4739a553fef00eee7dc13fa11c853d5772c2a8fc
Migbari <webdevel.bin@gmail.com>
2020-09-08 10:38:03 -0300

    Melhorando Implementação Imprimir Tabuleiro

--- Excer/TreinaTudoXadrez/Tela.cs
+++ Excer/TreinaTudoXadrez/Tela.cs
@@ -9,18 +9,35 @@ namespace TreinaTudoXadrez
         {
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " "); // Imprime numero linhas
                 for (int j = 0; j < tab.colunas; j++)
-                {
+                {   
+
                     if (tab.Peca(i,j) == null)
                     {
                         Console.Write("- ");
                     }
                     else
                     {
-                        Console.Write(tab.Peca(i, j) + " ");
+                        imprimirPeca(tab.Peca(i, j));
+                        Console.Write(" ");
                     }
-                }
+                }   
                 Console.WriteLine();
+            }
+            Console.WriteLine("  A B C D E F G H");
+        }
+        // para mudar a cor da preta
+        public static void imprimirPeca(Peca peca) { 
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            } else
+            {
+                ConsoleColor aux = Console.ForegroundColor; // guarda padrao em aux
+                Console.ForegroundColor = ConsoleColor.Yellow; // primeiro plano yellow
+                Console.Write(peca);
+                Console.ForegroundColor = aux; // volta ao padrao
             }
         }
     }
