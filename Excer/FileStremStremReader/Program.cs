94df629856c44b3b15718c8ecb9cb790fb30304e
Migbari <webdevel.bin@gmail.com>
2020-09-11 10:58:48 -0300

    FileStream e StreamReader

---
+++ Excer/FileStremStremReader/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace FileStremStremReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\webde\Desktop\Teste.txt";
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ocorreu");
+                Console.WriteLine(e.Message);
+            } 
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
