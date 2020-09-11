d0fb013e319e8fc1ae6d4bd19584cf00ec4f796e
Migbari <webdevel.bin@gmail.com>
2020-09-11 13:54:06 -0300

    StreamWriter  - Cópia de arquivo para outro com UpperCase

---
+++ Excer/StreamWriter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace streamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Program que copia escrita de um arquivo para outro em UpperCase
+            string sourcepath = @"C:\Users\webde\Desktop\Teste.txt";
+            string targetpath = @"C:\Users\webde\Desktop\Teste2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcepath);
+
+                using (StreamWriter sw = File.AppendText(targetpath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
