870057c0fd831dafb0dcd54ee9730a9cbe865e08
Migbari <webdevel.bin@gmail.com>
2020-09-30 10:15:47 -0300

    Directory and DirectoryInfo

---
+++ Excer/DirectoryAndDirectoryInfo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryAndDirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Programa que lista arquivos de um determinado diretorio
+            string path = @"C:\Users\webde\Desktop\diretorio";
+
+            try
+            {
+                // Lembrar da inferencia de tipo = detecção automática do tipo que a variável recebe
+                // ↓ ou var folders... retorna coleção IEnumerable strings das pastas / *.* to all files . all extensions
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories); // ← ou .TopDirectoryOnly
+                Console.WriteLine("FOLDERS: ");
+                foreach (string fo in folders)
+                {
+                    Console.WriteLine(fo);
+                } 
+                // Listar todos os arquivos de um diretorio ou subdiretorio
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories); // ← ou .TopDirectoryOnly
+                Console.WriteLine("FILES: ");
+                foreach (string fi in files)
+                {
+                    Console.WriteLine(fi);
+                }
+                // Cria nova pasta
+                Directory.CreateDirectory(path + @"\PastaNova");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu erro");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
