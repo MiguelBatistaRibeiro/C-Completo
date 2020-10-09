7d2db862dd7067b157df941553d5c20733720b28
Migbari <webdevel.bin@gmail.com>
2020-10-09 15:45:43 -0300

    Path de arquivos

---
+++ Excer/TrabalhandoPath/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace TrabalhandoPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Users\user\Desktop\arquivo.txt";
+
+            Console.WriteLine("Caracter separação de pastas: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("Caracter separação de paths: " + Path.PathSeparator);
+            Console.WriteLine("Caminho do arquivo: " + Path.GetDirectoryName(path));
+            Console.WriteLine("Arquivo - com extensão: " + Path.GetFileName(path));
+            Console.WriteLine("Extensão do arquivo: " + Path.GetExtension(path));
+            Console.WriteLine("Arquivo - sem extensão: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Caminho completo: " + Path.GetFullPath(path));
+            Console.WriteLine();
+            Console.WriteLine("Caminho arquivos temporários do sistema: ");
+            Console.Write(Path.GetTempPath());
+
+            Console.ReadLine();
+        }
+    }
+}
--- Excer/TrabalhandoPath/Properties/AssemblyInfo.cs
+++ Excer/TrabalhandoPath/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("UsingBlock")]
+[assembly: AssemblyTitle("TrabalhandoPath")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UsingBlock")]
+[assembly: AssemblyProduct("TrabalhandoPath")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("f7db43ad-b4d6-4037-a49c-bc0946b33665")]
+[assembly: Guid("fa0102be-d015-409e-8c86-016413142687")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
