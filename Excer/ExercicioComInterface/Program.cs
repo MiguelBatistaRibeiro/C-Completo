661078000021d1c207403325ab2ee54248cb22fb
Migbari <webdevel.bin@gmail.com>
2020-10-09 17:00:13 -0300

    Exercicio com Interface

---
+++ Excer/ExercicioComInterface/Program.cs
@@ -0,0 +1,35 @@
+﻿using ExercicioComInterface.Entities;
+using ExercicioComInterface.Services;
+using System;
+using System.Globalization;
+
+namespace ExercicioComInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do aluguel");
+            Console.Write("Modelo do carro: ");
+            string model = Console.ReadLine();
+            Console.Write("Retirada (dd/MM/yyyy HH:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Devolução (dd/MM/yyyy HH:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Preço por hora? ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Preço por dia? ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(hour, day, new BrazilTaxService());
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("Fatura:");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
---
+++ Excer/ExercicioComInterface/Services/RentalService.cs
@@ -0,0 +1,42 @@
+﻿using ExercicioComInterface.Entities;
+using System;
+
+namespace ExercicioComInterface.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        // inversão de controle por meio de injeção de dependencia. Não recebe mais a dependencia de BrazilTaxService
+        // recebe objeto instanciado e concede-o
+        private ITaxService _taxService;
+
+        public RentalService(double pricePerHour, double pricePerDay, ITaxService taxService)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+            _taxService = taxService;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start); // hra final - hra inicial
+            // define valor pagamento pela hora ou dia
+            double basicPayment = 0.0;
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours); // Ceilling - arredonda +
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+            // gera fatura obtendo basicPayment e adiciona em prop (Invoice - tipo Invoice) de CarRental
+            double tax = _taxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
