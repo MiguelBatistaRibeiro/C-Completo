20e6d285d172a3e55e11d3306e2a996bc3c5ec3d
Migbari <webdevel.bin@gmail.com>
2020-10-13 16:41:19 -0300

    Pjr Generics

---
+++ Excer/GenericsPjr/PrintService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GenericsPjr
+{
+    class PrintService<T>
+    {   
+        // T - substitui int ou string - sem type-safety
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintService está cheio");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+        public T First()
+        {
+            if(_count == 0)
+            {
+                throw new InvalidOperationException("PrintService está vazio");
+            }
+            return _values[0];
+        }
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count -1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if(_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ Excer/GenericsPjr/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericsPjr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("Quantos valores? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Digite posição {i}: ");
+                int x = int.Parse(Console.ReadLine()); // se elementos for string (ñ typesafety) - cria outra classe
+                printService.AddValue(x);
+            }
+            printService.Print();
+            Console.WriteLine("Primeiro: " + printService.First());
+
+            Console.ReadLine();
+        }
+    }
+}
