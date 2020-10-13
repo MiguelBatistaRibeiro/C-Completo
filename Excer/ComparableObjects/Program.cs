32906bb4c39cca188cfe0ee6fc9ca821f206951d
Migbari <webdevel.bin@gmail.com>
2020-10-13 15:39:32 -0300

    Interface IComparable - Comparação de obj

---
+++ Excer/ComparableObjects/Program.cs
@@ -0,0 +1,32 @@
+﻿using ComparableObjects.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ComparableObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Usuario\caminho\caminho\TextFile1.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort(); // usa IComparable. Um obj deve implementar IComparable. Employee Implementa
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro: ");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
