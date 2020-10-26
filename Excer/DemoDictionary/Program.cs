f59dbe2a53485139254d2d1a0bf616ce6b63f837
Migbari <webdevel.bin@gmail.com>
2020-10-26 11:58:43 -0300

    DemoDictionary

---
+++ Excer/DemoDictionary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99771122"; 
+            cookies["phone"] = "99771133"; // novo valor key
+
+            Console.WriteLine(cookies["email"]);
+            cookies.Remove("email");
+
+            Console.WriteLine("Número telefone: " + cookies["phone"]);
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine("Email: " + cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("Não há chave 'email'");
+            }
+
+            Console.WriteLine("Tamanho: " + cookies.Count);
+            Console.WriteLine("TOTAL COOKIES:");
+
+            foreach (KeyValuePair<string, string> item in cookies) // ou var
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
