9f18e01bc6fb1509e4f04869cf47624cab303430
Migbari <webdevel.bin@gmail.com>
2020-11-04 16:29:29 -0300

    Exer Dictionary e Extensions Method

---
+++ Excer/DictionaryCandidateVote/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DictionaryCandidateVote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o caminho do arquivo: "); // Formato: Miguel Bat, 15
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+
+                        string[] votingRecord = sr.ReadLine().Split(',');
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine(item.Key + ": " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro encontrado");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Excer/ExtensionMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using ExtensionMethods.Extensions;
+using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2020, 11, 04, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+            Console.WriteLine();
+
+            String str = "Cortando se maior que 20";
+            Console.WriteLine(str.Cut(20));
+            Console.ReadLine();
+        }
+    }
+}
